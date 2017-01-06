49882292145612486855535258a28d026f77448b
lindanqinghua <lindanqinghua@gmail.com>
2017-01-06 10:14:39 -0800

    add map

--- react/src/App.js
+++ react/src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {Map, Marker} from 'google-maps-react'
 
 class App extends Component {
   render() {
+    const logo = "./bayarea.jpg";
     var positions = this.props.positions;
+
     return (
       <div className="App">
-
         <div className="App-header">
           <img src={logo} 
             className="App-logo" 
             alt="logo" />
-
-          <h2>Live in Bay Area</h2>
+          <h2 style={{"margin": "10px"}}>Live in Bay Area</h2>
         </div>
 
         <FilterableMap positions={positions} />
@@ -24,42 +23,93 @@ class App extends Component {
 }
 
 class FilterableMap extends Component {
-  render() {
-    var positions = this.props.positions;
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-2 col-md-2">
-              <SearchBar />
-              <br/>
-              <PositionList positions={positions}/>
-          </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      showVisited: true,
+      showToVisit: true
+    };
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.handleToVisitCheckBox = this.handleToVisitCheckBox.bind(this);
+    this.handleVisitedCheckBox = this.handleVisitedCheckBox.bind(this);
+  }
 
-          <div className="col-sm-10 col-md-10">
-          </div>
+  handleUserInput(filterText) {
+    this.setState({filterText: filterText});
+  }
+
+  handleToVisitCheckBox() {
+    this.setState({showToVisit: !this.state.showToVisit});
+  }
+
+  handleVisitedCheckBox() {
+    this.setState({showVisited: !this.state.showVisited});
+  }
+
+  render() {
+    var positions = this.props.positions.filter((pos) => pos.name.toLowerCase()
+                                        .startsWith(this.state.filterText.toLowerCase()));
+
+    if(!this.state.showToVisit) {
+      positions = positions.filter((pos) => !pos.visit);
+    }
+
+    if(!this.state.showVisited) {
+      positions = positions.filter((pos) => pos.visit);
+    }
+
+    return (
+      <div className="mapContainer">
+        <div className="content side"> 
+           <SearchBar 
+            filterText={this.state.filterText}
+            onUserInput={this.handleUserInput}
+            onToVisitCheckbox={this.handleToVisitCheckBox}
+            onVisitedCheckbox={this.handleVisitedCheckBox}/>
+
+          <PositionList positions={positions}/>
+        </div>
+
+        <div className="content map"> 
+            <MyMap google={window.google} positions={positions}/>
         </div>
       </div>  
     );
   }
 }
 
-class SearchBar extends Component {
-  render() {
+class SearchBar extends Component { 
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.onUserInput(event.target.value);
+  }
+
+  render() {   
     return (
-     <form>
+     <form style={{"padding": "10px"}}>
       <div className="checkbox"> 
             <label>
-              <input type="checkbox"/> show visited 
+              <input type="checkbox"
+                onChange={this.props.onVisitedCheckbox}/> show visited 
             </label>
       </div>
+
       <div className="checkbox"> 
             <label>
-              <input type="checkbox"/> show to visit 
+              <input type="checkbox" 
+                onChange={this.props.onToVisitCheckbox}/> show to visit 
             </label>
       </div>
+
       <input type="text" 
         className="form-control" 
-        placeholder="Search…" />
+        placeholder="Search…" 
+        onChange={this.handleChange}/>
      </form>
     );
   }
@@ -83,6 +133,25 @@ class PositionList extends Component {
   }
 }
 
+class MyMap extends Component {
+  render() {
+    var google = this.props.google;
+    var positions = this.props.positions;
+    var markers = positions.map((pos, index) => 
+      <Marker key={index}
+        name={pos.name}
+        position={{lat: pos.latitude, lng: pos.longitude}}>
+      </Marker>
+      );
 
+    return (
+        <Map google={google} 
+          zoom={10} 
+          className="myMap">
+          {markers}
+        </Map>
+    );
+  }
+}
 
 export default App;
